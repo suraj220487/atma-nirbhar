fdeac61bbeee9a9ddd78b0e676e42afc41b2186c
Sarath Kaul <kaul.sarath@gmail.com>
2021-02-25 17:57:28 +0530

    Speech Changes

--- App.js
+++ App.js
@@ -50,141 +50,6 @@ function cacheImages(images) {
 }
 
 
-// export default props => {
-//   const [isLoadingComplete, setLoading] = useState(false);
-//   let [fontsLoaded] = useFonts({
-//     'ArgonExtra': require('./assets/font/argon.ttf'),
-//   });
-//   this.state = {
-//     messages: [
-//       {
-//         _id: 1,
-//         text: `Hi! Welcome to Atmanirbhar`,
-//         createdAt: new Date(),
-//         user: {
-//           _id: 2,
-//           name: 'Botler',
-//           avatar: "",
-//         }// <= note this
-//       }
-//     ]
-//   };
-
-//   function _loadResourcesAsync() {
-//     return Promise.all([...cacheImages(assetImages)]);
-//   }
-
-//   function dialogFlowIntegration() {
-//     const ACCESS_TOKEN = dialogflowConfig.access_token;
-
-//     try {
-//        const response = fetch(`https://dialogflow.googleapis.com/v2/projects/abcd-303017/agent/sessions/123:detectIntent`, {
-//         method: 'POST',
-//         headers: {
-//           'Accept': 'application/json',
-//           'Content-Type': 'application/json; charset=utf-8',
-//           'Authorization': `Bearer ${ACCESS_TOKEN}`,
-//         },
-//         body: JSON.stringify({
-//           "query_input": {
-//             "text": {
-//               "text": "What can you do",
-//               "language_code": "en-US"
-//             }
-//           }
-//         })
-//       }).then((response) => response.json()).then((json) => {
-//         console.log(json.queryResult.fulfillmentText);
-//         Speech.stop();
-
-//         Speech.speak(json.queryResult.fulfillmentText);
-//     }).catch((error) => {
-//         console.error(error);
-//     });
-//       // let responseJson = response.json();
-//       // console.log(responseJson);
-//   }catch(error) {
-//   console.error(error);
-// }
-
-// }
-
-//   function _handleLoadingError(error) {
-//     // In this case, you might want to report the error to your error
-//     // reporting service, for example Sentry
-//     console.warn(error);
-//   };
-
-//  function _handleFinishLoading() {
-//     setLoading(true);
-//   };
-
-//   if(!fontsLoaded && !isLoadingComplete) {
-//     return (
-//       <AppLoading
-//         startAsync={_loadResourcesAsync}
-//         startAsync={dialogFlowIntegration}
-//         onError={_handleLoadingError}
-//         onFinish={_handleFinishLoading}
-//       />
-//     );
-//   } else if(fontsLoaded) {
-//     return (
-//       <NavigationContainer>
-//         <GalioProvider theme={argonTheme}>
-//           <Block flex>
-//             <Screens />
-//           </Block>
-//         </GalioProvider>
-//       </NavigationContainer>
-//     );
-//   } else {
-//     return null
-//   }
-// }
-
-// export default class App extends React.Component {
-//   state = {
-//     isLoadingComplete: false
-//   };
-
-//   render() {
-//     if (!this.state.isLoadingComplete) {
-//       return (
-//         <AppLoading
-//           startAsync={this._loadResourcesAsync}
-//           onError={this._handleLoadingError}
-//           onFinish={this._handleFinishLoading}
-//         />
-//       );
-//     } else {
-//       return (
-//         <NavigationContainer>
-//           <GalioProvider theme={argonTheme}>
-//             <Block flex>
-//               <Screens />
-//             </Block>
-//           </GalioProvider>
-//         </NavigationContainer>
-//       );
-//     }
-//   }
-
-//   _loadResourcesAsync = async () => {
-//     return Promise.all([...cacheImages(assetImages)]);
-//   };
-
-//   _handleLoadingError = error => {
-//     // In this case, you might want to report the error to your error
-//     // reporting service, for example Sentry
-//     console.warn(error);
-//   };
-
-//   _handleFinishLoading = () => {
-//     this.setState({ isLoadingComplete: true });
-//   };
-// }
-
 
 let window = Dimensions.get('window');
 const contentHeight = window.height - 80;
@@ -273,7 +138,7 @@ export default class GiftedChatApp extends Component {
     ]
     console.log(answers);
     Speech.stop()
-    Speech.speak(result.queryResult.fulfillmentText)
+    Speech.speak(result.queryResult.fulfillmentText,{"rate":0.88})
 
     this.setState(previousState => ({
       gifted: GiftedChat.append(previousState.gifted, answers),
@@ -282,7 +147,7 @@ export default class GiftedChatApp extends Component {
   }
 
   async getDialogFlow(msg) {
-    const ACCESS_TOKEN = dialogflowConfig.access_token;
+    // const ACCESS_TOKEN = dialogflowConfig.access_token;
     
     try {
       Dialogflow_V2.requestQuery(msg, result=> this.handleGoogleResponse(result)
